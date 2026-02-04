190ff06ff6fdb804cf00b767e4cb89c892ff379d
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2026-02-04 13:36:16 +0530

    fix: conflicts

--- shell-app/vite.config.ts
+++ shell-app/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    federation({
+      name: 'shellApp',
+      remotes:{
+        authMF : 'https://auth-qdl5xb6rh-inshiya-ravats-projects.vercel.app/assets/authRemoteEntry.js'
+      },
+      shared: [ 'react', 'react-dom']
+    })
+  ],
 })
