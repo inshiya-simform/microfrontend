757742fa832ba9c325ccd424b1d845df5936d879
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2026-02-05 14:18:50 +0530

    feat: add product catalog link in home

--- shell-app/src/components/Home/Home.tsx
+++ shell-app/src/components/Home/Home.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 function Home() {
   return (
     <div>
       Welcome to the shell app
+      <br/>
+      <Link to="/products">Go to Product Catalog</Link>
     </div>
   )
 }
